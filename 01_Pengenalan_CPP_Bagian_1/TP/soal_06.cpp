5a031b429423fbc98bc061429302c1efecc99661
mfathirafa <mfathirafa@gmail.com>
2024-09-29 21:43:26 +0700

    Add files via upload

---
+++ 01_Pengenalan_CPP_Bagian_1/TP/soal_06.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, bilangan;
+    cout << "Masukkan batas bawah: ";
+    cin >> a;
+    cout << "masukkan batas atas: ";
+    cin >> b;
+    for (bilangan = a; bilangan <= b; bilangan++) {
+        cout << "bilangan " << bilangan << endl;
+    }
+    return 0;
+}
