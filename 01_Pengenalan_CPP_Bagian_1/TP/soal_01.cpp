1826dd7d8d01527a9f79c81d652ca5967f730f5a
mfathirafa <mfathirafa@gmail.com>
2024-09-29 21:38:47 +0700

    Add files via upload

---
+++ 01_Pengenalan_CPP_Bagian_1/TP/soal_01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama, nim;
+    cout << "siapa nama anda?";
+    cin >> nama;
+    cout << "berapa nim anda?";
+    cin >> nim;
+    cout << "nama saya:" << nama << endl;
+    cout << "siapa nama anda:" << nim << endl;
+    return 0;
+}
