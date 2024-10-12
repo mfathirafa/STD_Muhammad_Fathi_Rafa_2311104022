5978f2bc9408fedcce21460979ff1bf5ef1dae07
mfathirafa <mfathirafa@gmail.com>
2024-10-12 21:58:36 +0700

    Add files via upload

---
+++ 03_Abstract_Data_Type/UNGUIDED/LATIHAN_02/main.cpp
@@ -0,0 +1,9 @@
+#include "pelajaran.h"
+
+int main() {
+    string namapel = "Struktur Data";
+    string kodepel = "STD";
+    pelajaran pel = create_pelajaran(namapel, kodepel);
+    tampil_pelajaran(pel);
+    return 0;
+}
---
+++ 03_Abstract_Data_Type/UNGUIDED/LATIHAN_02/pelajaran.cpp
@@ -0,0 +1,15 @@
+#include "pelajaran.h"
+#include <iostream>
+using namespace std;
+
+pelajaran create_pelajaran(string namaMapel, string kodeMapel) {
+    pelajaran pel;
+    pel.namaMapel = namaMapel;
+    pel.kodeMapel = kodeMapel;
+    return pel;
+}
+
+void tampil_pelajaran(pelajaran pel) {
+    cout << "Nama pelajaran: " << pel.namaMapel << endl;
+    cout << "Kode pelajaran: " << pel.kodeMapel << endl;
+}
