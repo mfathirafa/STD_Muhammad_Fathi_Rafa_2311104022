fcb3ad9fc7a5e0508ba82f5e70149c29355e6525
mfathirafa <mfathirafa@gmail.com>
2024-10-12 21:42:27 +0700

    Add files via upload

---
+++ 03_Abstract_Data_Type/UNGUIDED/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Mahasiswa {
+    string nama;
+    string nim;
+    float uts;
+    float uas;
+    float tugas;
+    float nilaiAkhir;
+};
+
+float hitungNilaiAkhir(float uts, float uas, float tugas) {
+    return (0.3 * uts) + (0.4 * uas) + (0.3 * tugas);
+}
+
+int main() {
+    const int MAX_MAHASISWA = 10;
+    Mahasiswa mahasiswa[MAX_MAHASISWA];
+    int jumlahMahasiswa;
+
+    cout << "Masukkan jumlah mahasiswa (maksimal 10): ";
+    cin >> jumlahMahasiswa;
+
+    if (jumlahMahasiswa > MAX_MAHASISWA) {
+        cout << "Jumlah mahasiswa melebihi batas maksimal!" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < jumlahMahasiswa; ++i) {
+        cout << "Masukkan data mahasiswa ke-" << i + 1 << ":" << endl;
+        cout << "Nama: ";
+        cin >> mahasiswa[i].nama;
+        cout << "NIM: ";
+        cin >> mahasiswa[i].nim;
+        cout << "Nilai UTS: ";
+        cin >> mahasiswa[i].uts;
+        cout << "Nilai UAS: ";
+        cin >> mahasiswa[i].uas;
+        cout << "Nilai Tugas: ";
+        cin >> mahasiswa[i].tugas;
+
+        mahasiswa[i].nilaiAkhir = hitungNilaiAkhir(mahasiswa[i].uts, mahasiswa[i].uas, mahasiswa[i].tugas);
+    }
+
+    cout << "\nData Mahasiswa:\n";
+    for (int i = 0; i < jumlahMahasiswa; ++i) {
+        cout << "Mahasiswa ke-" << i + 1 << ":" << endl;
+        cout << "Nama: " << mahasiswa[i].nama << endl;
+        cout << "NIM: " << mahasiswa[i].nim << endl;
+        cout << "Nilai UTS: " << mahasiswa[i].uts << endl;
+        cout << "Nilai UAS: " << mahasiswa[i].uas << endl;
+        cout << "Nilai Tugas: " << mahasiswa[i].tugas << endl;
+        cout << "Nilai Akhir: " << mahasiswa[i].nilaiAkhir << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
